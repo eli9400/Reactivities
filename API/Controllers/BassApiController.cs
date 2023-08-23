e6631de4ee69d3a6bf932f752ab68e3555a7cfdb
eli9400 <121621986+eli9400@users.noreply.github.com>
2023-08-23 09:41:32 +0300

    part 4

--- API/Controllers/BassApiController.cs
+++ API/Controllers/BassApiController.cs
@@ -1,10 +1,11 @@
-
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 { [ApiController]
 [Route("api/[controller]")]
     public class BassApiController: ControllerBase
     {
-        
+        private IMediator _mediator;
+        protected IMediator Mediator=>_mediator ??=HttpContext.RequestServices.GetService<IMediator>();
     }
 }
